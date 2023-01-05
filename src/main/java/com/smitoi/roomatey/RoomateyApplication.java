15950bf3dade72b727458ca915c254cbf27c1c6d
Mitoi Stefan - Daniel <stefan.mitoi27@gmail.com>
2023-01-06 01:25:11 +0200

    Initial commit

---
+++ src/main/java/com/smitoi/roomatey/RoomateyApplication.java
@@ -0,0 +1,13 @@
+package com.smitoi.roomatey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomateyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomateyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smitoi/roomatey/RoomateyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smitoi.roomatey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomateyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
